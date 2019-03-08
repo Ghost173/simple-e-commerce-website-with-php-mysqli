f818fc74224779b1ba425c1d3d727a1c5f99b312
jpirakas007@gmail.com <ghost173>
2019-03-08 16:17:53 +0530

    add search funcations

--- main.js
+++ main.js
@@ -39,5 +39,54 @@ $(document).ready(function(){
 				
 			}
 		})
-	}
+   }
+   
+//categort in side menu
+   $("body").delegate(".category","click",function(event){
+		event.preventDefault();
+		var cid = $(this).attr('cid');
+		//alert(cid);
+			$.ajax({
+			url		:	"action.php",
+			method	:	"POST",
+			data	:	{get_seleted_Category:1,cat_id:cid},
+			success	:	function(data){
+				$("#get_product").html(data);
+				
+			}
+		})
+	
+   })
+   
+   // brand in side menu
+   $("body").delegate(".selectbrand","click",function(event){
+		event.preventDefault();
+		var bid = $(this).attr('bid');
+		//alert(cid);
+			$.ajax({
+			url		:	"action.php",
+			method	:	"POST",
+			data	:	{selectbrand:1,brand_id:bid},
+			success	:	function(data){
+				$("#get_product").html(data);
+				
+			}
+		})
+	
+   })
+   //sarch
+  $("#search_btn").click(function(){
+   var keyword = $("#search").val();
+     if (keyword != "") {
+      $.ajax({
+			url		:	"action.php",
+			method	:	"POST",
+			data	:	{search:1,keyword:keyword},
+			success	:	function(data){
+				$("#get_product").html(data);
+				
+			}
+		})
+     }
+  })
 })
