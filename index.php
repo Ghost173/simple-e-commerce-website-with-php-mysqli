368c7aac9b5ee63ab7652107e1e5f3b98f3dfff1
jpirakas007@gmail.com <ghost173>
2019-03-11 16:07:12 +0530

    session added

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 ini_set('display_startup_errors',1);
 ini_set('display_errors',1);
 error_reporting(-1);
+session_start();
+if($_SESSION["uid"])
+{
+    header("location:profile.php");
+}
 ?>
 
 <!DOCTYPE html>
