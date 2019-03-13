799ec3dcd146e243913b7f729a33ee76c54d57fe
jpirakas007@gmail.com <ghost173>
2019-03-13 16:41:39 +0530

    paypal payment fail  redrict to here

---
+++ cancel.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ONLINE STORE</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+   
+    <link rel="stylesheet" href="css/bootstrap.min.css"/>
+		<script src="js/jquery2.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="main.js"></script>
+		<link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+     integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" 
+     crossorigin="anonymous">
+
+     <style>
+     table tr td {padding:10px;}
+     </style>
+</head>
+<body>
+
+<div class="navbar navbar-inverse navbar-fixed-top"> 
+    <div class="container-fluid"> 
+        <div class="navbar navbar-header"> 
+            <a href="index.php" class="navbar-brand"> ONLINE STORE</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li> <a href="#"><i class="fas fa-home"></i>Home</a></li>
+            <li> <a href="#"><i class="fas fa-store"></i>Product</a></li>
+        </ul>
+        </div>
+        </div>
+
+<p><br/></p>
+<p><br/></p>
+<p><br/></p>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-8">
+        <div class="panel panel-primary">
+            <div class="panel-heading"></div>
+            <div class="panel-body">
+                <h1>Thankyou For Your Orders </h1>
+                <p>Hello  Your payment is cancel<br/>
+                retry with valid method thank you:D<br/>
+                </p>
+                <a href="index.php" class="btn btn-danger btn-lg">countinue shopping</a>
+                <hr/>
+            </div>
+            <div class="panel-headifooter"></div>
+        </div>
+        </div>
+        <div class="col-md-2"></div>
+    </div>
+</div>
+
+
+</body>
+</html>
