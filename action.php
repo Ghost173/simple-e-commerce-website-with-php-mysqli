84961741051bced03dd026856bea9a29cbd6ef12
jpirakas007@gmail.com <ghost173>
2019-03-13 16:37:59 +0530

    all php backend are here

--- action.php
+++ action.php
@@ -43,11 +43,38 @@ if(isset($_POST["brand"])){
 		echo "</div>";
 	}
 }
+// pagenation
+// if(isset($_POST["page"])){
+//     $sql = "SELECT * FROM products";
+//     $run_query = mysqli_query($con,$sql);
+//     $count = mysqli_num_rows($run_query);
+//     //echo $count ;
+//      $pageno = ceil($count / 9);
+//      for ($i=1; $i<=$pageno;$i++) {
+//         echo " <li><a href='#' page='$i' id='page'>1</a></li>";
+//      }
+//     }
+if(isset($_POST["page"])){
+	$sql = "SELECT * FROM products";
+	$run_query = mysqli_query($con,$sql);
+	$count = mysqli_num_rows($run_query);
+    $pageno = ceil($count/9);
+    
+	for($i=1;$i<=$pageno;$i++){
+		echo "
+			<li><a href='#' page='$i' id='page'>$i</a></li>
+		";
+	}
+}
+// get_product ORDER BY RAND ()
+if(isset($_POST["getproduct"])) {
+    $limit = 9;
+    if(isset($_POST["setpage"])) {
+       $pageno = $_POST["pageno"];
+       $start  = ($pageno * $limit) - $limit ;
+    }else { $start = 0;}
 
-
-// get_product
-if(isset($_POST["getproduct"]) ) {
-    $product_query = "SELECT * FROM products ORDER BY RAND () LIMIT 0,12";
+    $product_query = "SELECT * FROM products  LIMIT $start,$limit";
     $run_query = mysqli_query($con,$product_query);
 
     if(mysqli_num_rows($run_query) > 0) {
@@ -117,34 +144,46 @@ if(isset($_POST["get_seleted_Category"]) || isset($_POST["selectbrand"])  || iss
 }
 
 if(isset($_POST["addProduct"])){
-$p_id = $_POST["proId"];
-$user_id = $_SESSION["uid"];
-$sql = "SELECT * FROM cart WHERE p_id = '$p_id' AND user_id = '$user_id' ";
-$run_query = mysqli_query($con,$sql);
-$count = mysqli_num_rows($run_query);
-if ($count   > 0) {
-    echo "product is a;ready added";
-}else {
-    $sql = "SELECT * FROM products WHERE product_id='$p_id'";
-    $run_query = mysqli_query($con,$sql);
-    $row = mysqli_fetch_array($run_query);
+    if (isset($_SESSION["uid"])) {
+        $p_id = $_POST["proId"];
+        $user_id = $_SESSION["uid"];
+        $sql = "SELECT * FROM cart WHERE p_id = '$p_id' AND user_id = '$user_id' ";
+        $run_query = mysqli_query($con,$sql);
+        $count = mysqli_num_rows($run_query);
+        if ($count   > 0) {
+            echo "product is a;ready added";
+        }else {
+            $sql = "SELECT * FROM products WHERE product_id='$p_id'";
+            $run_query = mysqli_query($con,$sql);
+            $row = mysqli_fetch_array($run_query);
+        
+            $id=  $row["product_id"];
+            $pro_name=$row["product_title"];
+            $pro_image=$row["product_image"];
+            $pro_price=$row["product_price"];
+            
+        $sql="INSERT INTO `cart` (`id`, `p_id`, `ip_add`, `user_id`, `qty`, `product_title`, `product_image`, `price`, `total_amt`) 
+        VALUES (NULL, '$p_id', '0', '$user_id', '1', '$pro_name', '$pro_image', '$pro_price', '$pro_price');";
+        
+        if(mysqli_query($con,$sql)) {
+            echo "
+            <div class='alert alert-success'>
+            <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+            <b>Product added to cert :)</b>
+            </div>
+            ";
+        }
+        }
 
-    $id=  $row["product_id"];
-    $pro_name=$row["product_title"];
-    $pro_image=$row["product_image"];
-    $pro_price=$row["product_price"];
-    
-$sql="INSERT INTO `cart` (`id`, `p_id`, `ip_add`, `user_id`, `qty`, `product_title`, `product_image`, `price`, `total_amt`) 
-VALUES (NULL, '$p_id', '0', '$user_id', '1', '$pro_name', '$pro_image', '$pro_price', '$pro_price');";
+    }else {
+       echo "
+       <div class='alert alert-danger'>
+       <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+       <b>please SIGN in or register to add to cart</b>
+       </div>   
+       ";
+    }
 
-if(mysqli_query($con,$sql)) {
-    echo "
-    <div class='alert alert-success'>
-    <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a><b>Product added to cert :)</b>
-    </div>
-    ";
-}
-}
 }
 
 
@@ -156,6 +195,7 @@ if (isset($_POST["get_cart_product"]) || isset($_POST["car_checkout"])){
     $count = mysqli_num_rows($run_query);
     if ($count > 0 ) {
         $no =1;
+        $total_amt =0;
         while ($row = mysqli_fetch_array($run_query)) {
             $id = $row["id"];
             $pro_id =$row["p_id"];
@@ -164,6 +204,9 @@ if (isset($_POST["get_cart_product"]) || isset($_POST["car_checkout"])){
             $pro_price=$row["price"];
             $qty = $row["qty"];
             $total = $row["total_amt"];
+            $price_array = array($total);
+            $total_sum = array_sum($price_array);
+            $total_amt = $total_amt +$total_sum ;
 
             if (isset($_POST["get_cart_product"])) {
                 echo "
@@ -197,7 +240,58 @@ if (isset($_POST["get_cart_product"]) || isset($_POST["car_checkout"])){
 
           
         }
+if (isset($_POST["car_checkout"])) {
+            echo "
+            <div class='row'>
+            <div class='col-md-8'></div>
+            <div class='col-md-4'>
+            <b>Total:$total_amt</b>
+            </div>
+        </div>";
+        }
+        echo '
+        <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
+            <input type="hidden" name="cmd" value="_cart">
+            <input type="hidden" name="business" value="jpshadow007@gmail.com">
+            <input type="hidden" name="upload" value="1">';
+
+            $x=0;
+            $uid =$_SESSION["uid"];
+            $sql = "SELECT * FROM cart WHERE user_id='$uid'";
+            $run_query = mysqli_query($con,$sql);
+            
+
+            while($row=mysqli_fetch_array($run_query)) {
+                $x++;
+                echo'
+            
+            <input type="hidden" name="item_name_'.$x.'" value="'.$row["product_title"].'">
+            <input type="hidden" name="item_number_'.$x.'" value="'.$x.'">
+            <input type="hidden" name="amount_'.$x.'" value="'.$row["price"].'">
+            <input type="hidden" name="quantity_'.$x.'" value="'.$row["qty"].'">
+            
+            ';
+            
+            }
+
+        echo '    
+            <input type="hidden" name="return" value="http://127.0.0.1/htmlcss/smok/payment_success.php">
+            <input type="hidden" name="cancel_return" value="http://127.0.0.1/htmlcss/smok/cancel.php">
+            <input type="hidden" name="currency_code" value="USD"/>
+            <input type="hidden" name="custom" value="'.$uid.'"/>
+            <input style="float:right;margin-right:100px" type="image" name="submit"
+            src="https://www.paypalobjects.com/webstatic/en_US/i/btn/png/blue-rect-paypalcheckout-60px.png"
+              alt="PayPal - The safer, easier way to pay online">
+          </form>
+        ';
     }
+}
+// cart count 
+if (isset($_POST["cart_count"])) {
+    $uid = $_SESSION["uid"];
+    $sql = "SELECT * FROM cart WHERE user_id='$uid'";
+    $run_query = mysqli_query($con,$sql);
+    echo mysqli_num_rows($run_query);
 }
 //cart delete
 if(isset($_POST["removefromcart"])) {
@@ -207,10 +301,36 @@ $sql = "DELETE FROM cart WHERE user_id = '$uid' AND p_id ='$pid' ";
 $run_quey = mysqli_query($con,$sql);
 if($run_quey) {
     echo "
-    <div class='alert alert-success'>
+    <div class='alert alert-primary'>
     <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a><b>iteam remove from cart</b>
     </div>
     ";
 }
 }
+
+//update cart qty
+if (isset($_POST["updatproduct"])){
+    $uid = $_SESSION["uid"];
+    $pid = $_POST["updateid"];
+    $qty = $_POST["qty"];
+    $price = $_POST["price"];
+    $total = $_POST["total"];
+
+    $sql = "UPDATE cart SET qty = '$qty',price='$price', total_amt='$total' 
+    WHERE user_id = '$uid' AND p_id='$pid'";
+    
+    $run_query = mysqli_query($con,$sql);
+
+    if ($run_query) {
+        echo "
+        <div class='alert alert-success'>
+        <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a><b>iteam update success</b>
+        </div>  
+        ";
+    }
+}
+
+//pagenination
+
+
 ?>
